a7abb50a519105ed0f6b9f59739304eeb680d869
Amer Itani <itaniamer4@gmail.com>
2025-09-30 13:24:40 +0300

    Adding quantity and discount to total cost

--- grocery.py
+++ grocery.py
@@ -5,17 +5,26 @@ groceries = {
     "bread": 2
 }
 shopping_cart = []
+cart = {}
 while True:
-    item = input("What do you want to buy? ")
+    item_quantity = input("What do you want to buy and how many? ").split()
+    item = item_quantity[0]
     if item == "done":
         break
-    elif item in groceries:
-        shopping_cart.append(item)
-    else:
+    elif len(item_quantity) !=2:
+        print("Please enter item and quantity respectively seperated by a whitespace.")
+    elif item not in groceries:
         print("Sorry, we don't have that item.")
+    else:
+        quantity = int(item_quantity[1])
+        shopping_cart.append(item)
+        cart[item] = quantity
 total_cost = 0
 for item in shopping_cart:
-    total_cost += groceries[item]
+    total_cost += groceries[item] * cart[item]
+if "milk" in cart:
+    if cart["milk"] > 2:
+        total_cost -= 1
 print("\nYou bought: " + str(shopping_cart) + "\nTotal = $" + str(total_cost))
 if total_cost > 10:
     print("You spent a lot!")
