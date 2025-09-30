36c869cf71e01f7c1a01f6c945c1dd3817901b8a
Amer Itani <itaniamer4@gmail.com>
2025-09-30 12:17:54 +0300

    Calculating total cost of bought items

--- grocery.py
+++ grocery.py
@@ -13,3 +13,11 @@ while True:
         shopping_cart.append(item)
     else:
         print("Sorry, we don't have that item.")
+total_cost = 0
+for item in shopping_cart:
+    total_cost += groceries[item]
+print("\nYou bought: " + str(shopping_cart) + "\nTotal = $" + str(total_cost))
+if total_cost > 10:
+    print("You spent a lot!")
+else:
+    print("You spent a little!")
